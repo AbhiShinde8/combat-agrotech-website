9e3f6a30cadbd416b14f36f9f0e186fe3c1bf12e
Abhijit <abshinde2811@gmail.com>
2025-12-26 16:24:42 +0530

    vite file change add base path in it

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
 
   return {
+    base: "/subfolder/", // ✅ Subfolder path
     server: {
       port: 3000,
       host: "0.0.0.0",
